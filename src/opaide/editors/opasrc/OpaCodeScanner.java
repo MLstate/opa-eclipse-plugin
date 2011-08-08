bce43d90172ba543e54d0e41ad892f3452171fd2
geoffroy <geoffroy.chollon@mlstate.com>
2011-08-08 17:57:08 +0200

    continuing refactoring in the code scanner

--- src/opaide/editors/opasrc/OpaCodeScanner.java
+++ src/opaide/editors/opasrc/OpaCodeScanner.java
@@ -51,15 +51,18 @@ public class OpaCodeScanner extends RuleBasedScanner {
 			}
 			@Override
 			public ITextualRep[] getTextualReps() {
-				return null;
+				return new ITextualRep[]{};
 			}
 		};
+		
 		@Override
 		public String toString() {
 			return this.getClass().getCanonicalName() + "." + super.toString();
 		}
+		
 		public abstract IWordDetector getWordDetector();
 		public abstract ITextualRep[] getTextualReps();
+		
 	}
 	
 
@@ -68,39 +71,18 @@ public class OpaCodeScanner extends RuleBasedScanner {
 		
 		List<IRule> rules = new ArrayList<IRule>();
 		
-		{	SavedTextAttribute attrForKeyword = styleProvider.getSavedTextAttribute(CODE.KEYWORD);
-			IToken keywordToken = new Token(new TextAttribute( ColorManager.getColor(attrForKeyword.getColor())));
-			WordRule keywordRule = new WordRule(OPAKEYWORDS.getWordDetector());
-			for (ITextualRep k : OPAKEYWORDS.values()) {
+		for (CODE c : CODE.values()) {
+			SavedTextAttribute attrForKeyword = styleProvider.getSavedTextAttribute(c);
+			IToken keywordToken = new Token(new TextAttribute( ColorManager.getColor(attrForKeyword.getColor()) ));
+			ITextualRep[] values = c.getTextualReps();
+			WordRule keywordRule = 
+					(values.length == 0) ? new WordRule(c.getWordDetector(), keywordToken) : new WordRule(c.getWordDetector());
+			for (ITextualRep k : values) {
 				keywordRule.addWord(k.getTextRep(), keywordToken);
 			}
 			rules.add(keywordRule);
-		};
-		
-		{	SavedTextAttribute attrForSeparator = styleProvider.getSavedTextAttribute(CODE.SEPARATOR);
-			IToken separatorsToken = new Token(new TextAttribute( ColorManager.getColor(attrForSeparator.getColor())));
-			WordRule separatorsRule = new WordRule(OPASEPARATORS.getWordDetector());
-			for (OPASEPARATORS k : OPASEPARATORS.values()) {
-				separatorsRule.addWord(k.getTextRep(), separatorsToken);
-			}
-			rules.add(separatorsRule);
-		};
-		
-		{	SavedTextAttribute attrForGenericWord = styleProvider.getSavedTextAttribute(CODE.GENERIC_WORD);
-			IToken genericWordToken = new Token(new TextAttribute( ColorManager.getColor(attrForGenericWord.getColor())));
-			WordRule basicWordRule = new WordRule(new IWordDetector() {			
-				@Override
-				public boolean isWordStart(char c) {
-					return ((c=='_') || (c>='a' && c<='z') || (c>='A' && c<='Z'));
-				}			
-				@Override
-				public boolean isWordPart(char c) {
-					return ((c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='0' && c<='9') || c=='_');
-				}
-			}, genericWordToken);
-			rules.add(basicWordRule);
-		};
-		
+		}
+
 		IRule[] result = new IRule[rules.size()];
 		rules.toArray(result);
 		IToken defaultToken = new Token(new TextAttribute(ColorManager.getColor(new RGB(0, 255, 0))));
